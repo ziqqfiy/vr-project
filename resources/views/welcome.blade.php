ce1e509e38678fa673cce1136e22bb37530946f6
ziqqfiy <ziqq.fiy@gmail.com>
2021-11-30 21:27:30 +0800

    A-Frame Deploy

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>VR Test</title>
+
+    <!-- Script -->
+    <script src="https://aframe.io/releases/1.1.0/aframe.min.js"></script>
+
+    <!-- Fonts -->
+
+    <!-- Styles -->
+</head>
+
+<body>
+    <a-scene>
+        <a-box position="-1.5 1 -1.5" rotation="0 0 0" color="#98C379"></a-box>
+    </a-scene>
+</body>
+
+</html>
