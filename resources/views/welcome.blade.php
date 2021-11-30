55efc6bea1ae86d9b6477595c74a790ebfc7d27d
ziqqfiy <ziqq.fiy@gmail.com>
2021-11-30 23:50:19 +0800

    Obstacle Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,7 +19,7 @@
         <a-box height="5" width="4" depth="5" position="1.5 2.5 -90" animation="property: position; to:1.5 2.5 30; dur: 7000; easing: linear; loop: false" color="orange"></a-box>
         <a-box height="3" width="8" depth="5" position="0 3 -110" animation="property: position; to:0 3 10; dur: 7000; easing: linear; loop: false" color="orange"></a-box>
 
-        <a-plane position="0 0 0" rotation="-90 0 0" width="5" depth="5" height="5" color="white"></a-box>
+        <a-plane position="0 0 -40" rotation="-90 0 0" width="7" depth="5" height="90" color="white"></a-box>
 
         <a-sky color="#cbded4"></a-sky>
     </a-scene>
