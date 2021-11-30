7ebd6385e104a609da642fef3d46ead18c1f580a
ziqqfiy <ziqq.fiy@gmail.com>
2021-11-30 23:52:23 +0800

    Obstacle Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,11 +13,11 @@
 
 <body>
     <a-scene>
-        <a-box height="3" width="8" depth="5" position="0 3 -30" animation="property: position; to:0 3 90; dur: 7000; easing: linear; loop: false" color="orange"></a-box>
-        <a-box height="5" width="4" depth="5" position="1.5 2.5 -50" animation="property: position; to:1.5 2.5 70; dur: 7000; easing: linear; loop: false" color="orange"></a-box>
-        <a-box height="5" width="4" depth="5" position="-1.5 2.5 -70" animation="property: position; to:-1.5 2.5 50; dur: 7000; easing: linear; loop: false" color="orange"></a-box>
-        <a-box height="5" width="4" depth="5" position="1.5 2.5 -90" animation="property: position; to:1.5 2.5 30; dur: 7000; easing: linear; loop: false" color="orange"></a-box>
-        <a-box height="3" width="8" depth="5" position="0 3 -110" animation="property: position; to:0 3 10; dur: 7000; easing: linear; loop: false" color="orange"></a-box>
+        <a-box height="3" width="8" depth="5" position="0 3 -30" animation="property: position; to:0 3 90; dur: 7000; easing: linear; loop: true" color="orange"></a-box>
+        <a-box height="5" width="4" depth="5" position="1.5 2.5 -50" animation="property: position; to:1.5 2.5 70; dur: 7000; easing: linear; loop: true" color="orange"></a-box>
+        <a-box height="5" width="4" depth="5" position="-1.5 2.5 -70" animation="property: position; to:-1.5 2.5 50; dur: 7000; easing: linear; loop: true" color="orange"></a-box>
+        <a-box height="5" width="4" depth="5" position="1.5 2.5 -90" animation="property: position; to:1.5 2.5 30; dur: 7000; easing: linear; loop: true" color="orange"></a-box>
+        <a-box height="3" width="8" depth="5" position="0 3 -110" animation="property: position; to:0 3 10; dur: 7000; easing: linear; loop: true" color="orange"></a-box>
 
         <a-plane position="0 0 -40" rotation="-90 0 0" width="7" depth="5" height="90" color="white"></a-box>
 
