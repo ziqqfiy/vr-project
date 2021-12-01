5f4dcca18be0b9c149f939ff0f525a053a8c71e6
ziqqfiy <ziqq.fiy@gmail.com>
2021-12-01 10:02:46 +0800

    Level Tweaked

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,15 +19,21 @@
         <a-entity light="type: ambient; color: #BBB; castShadow: true"></a-entity>
         <a-entity light="type: directional; color: #FFF; intensity: 0.6 castShadow: true" position="-0.5 1 1"></a-entity>
 
-        <a-entity position="0 0 -10">
-            <a-box shadow="cast: true" height="3" width="8" depth="5" position="0 3 -30" animation="property: position; to:0 3 90; dur: 7000; easing: linear; loop: true" color="orange" material="opacity: 0.8; transparency: true"></a-box>
-            <a-box shadow="cast: true" height="5" width="4" depth="5" position="1.5 2.5 -50" animation="property: position; to:1.5 2.5 70; dur: 7000; easing: linear; loop: true" color="blue" material="opacity: 0.8; transparency: true"></a-box>
-            <a-box shadow="cast: true" height="5" width="4" depth="5" position="-1.5 2.5 -70" animation="property: position; to:-1.5 2.5 50; dur: 7000; easing: linear; loop: true" color="orange" material="opacity: 0.8; transparency: true"></a-box>
-            <a-box shadow="cast: true" height="5" width="4" depth="5" position="1.5 2.5 -90" animation="property: position; to:1.5 2.5 30; dur: 7000; easing: linear; loop: true" color="blue" material="opacity: 0.8; transparency: true"></a-box>
-            <a-box shadow="cast: true" height="3" width="8" depth="5" position="0 3 -110" animation="property: position; to:0 3 10; dur: 7000; easing: linear; loop: true" color="orange" material="opacity: 0.8; transparency: true"></a-box>
+        <a-entity position="0 0 -10" animation="property: position; to: 0 0 140; dur: 7000; easing: linear; loop: true">
+            <a-box shadow="cast: true" height="3" width="8" depth="5" position="0 3 -30" color="orange" material="opacity: 0.8; transparency: true"></a-box>
+            
+            <a-box shadow="cast: true" height="5" width="1.5" depth="5" position="0.75 2.5 -50" color="orange" material="opacity: 0.8; transparency: true"></a-box>
+            
+            <a-box shadow="cast: true" height="5" width="1.5" depth="5" position="-0.75 2.5 -70" color="orange" material="opacity: 0.8; transparency: true"></a-box>
+            
+            <a-box shadow="cast: true" height="5" width="1.5" depth="5" position="0.75 2.5 -90" color="orange" material="opacity: 0.8; transparency: true"></a-box>
+
+            <a-box shadow="cast: true" height="5" width="1.5" depth="5" position="-0.75 2.5 -110" color="orange" material="opacity: 0.8; transparency: true"></a-box>d
+           
+            <a-box shadow="cast: true" height="3" width="8" depth="5" position="0 3 -130" color="orange" material="opacity: 0.8; transparency: true"></a-box>
         </a-entity>
 
-        <a-plane shadow="receive: true" position="0 0 -40" rotation="-90 0 0" width="3" depth="5" height="90" color="white"></a-plane>
+        <a-plane shadow="receive: true" position="0 0 -44" rotation="-90 0 0" width="3" depth="5" height="90" color="white"></a-plane>
         <a-sky radius="50" color="#cbded4"></a-sky>
     </a-scene>
 </body>
