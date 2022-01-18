fe9b4d2f8bb9f324eb5b160a32fa0be5ef25a1ad
ziqqfiy <ziqq.fiy@gmail.com>
2022-01-18 13:55:09 +0800

    TailwindCSS Intergration

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./resources/views/dashboard.blade.php",
+    "./resources/views/auth/register.blade.php",
+    "./resources/views/auth/qrlogin.blade.php",
+    "./resources/views/auth/profile.blade.php",
+    "./resources/views/auth/login.blade.php",
+    "./resources/views/auth/layout.blade.php",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
